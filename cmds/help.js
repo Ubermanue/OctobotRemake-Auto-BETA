aceb8baf7f4bd9c6036bbe3fa690d694fd08c868
Ubermanue <rinz135orugejehe@gmail.com>
2024-06-21 14:43:17 +0800

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -1,7 +1,7 @@
 module.exports = {
     description: "Show Commands and the descriptions",
     role: "user",
-    credits: "rejardgwapo",
+    credits: "Vixvus",
     cooldown: 16,		
     execute(api, event, args, commands) {
         let helpMessage = '𝙷𝚒! 𝚃𝚑𝚒𝚜 𝙱𝚘𝚝 𝚒𝚜 𝚌𝚛𝚎𝚊𝚝𝚎𝚍 𝚊𝚝 https://octobotremakeauto.onrender.com/. 𝙷𝚎𝚛𝚎 𝚊𝚛𝚎 𝚊𝚕𝚕 𝚝𝚑𝚎 𝚌𝚘𝚖𝚖𝚊𝚗𝚍𝚜 𝚒𝚗𝚜𝚝𝚊𝚕𝚕𝚎𝚍 𝚘𝚗 𝚝𝚑𝚒𝚜 𝚜𝚎𝚛𝚟𝚎𝚛\n';
